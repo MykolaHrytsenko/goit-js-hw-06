b2a7bf83ffec4f75e44e1ff557b62ebb8c4e37bd
Mykola_Hrytsenko <94016020+MykolaHrytsenko@users.noreply.github.com>
2022-05-13 13:09:43 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsContainer = document.querySelector('ul#ingredients');
+
+const elements = ingredients.map(option => {
+  const ingrElement = document.createElement('li');
+  ingrElement.textContent = option;
+  ingrElement.classList.add('item');
+
+  return ingrElement;
+})
+
+ingredientsContainer.append(...elements);
+
+console.log(elements);
+
+
