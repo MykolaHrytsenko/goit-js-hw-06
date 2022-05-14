97625c2cc8229600534c351a565f4df3657909e5
MykolaHrytsenko <niksandler@gmail.com>
2022-05-14 18:32:53 +0300

    task 4 done

---
+++ js/task-04.js
@@ -0,0 +1,14 @@
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+let counterValue = 0;
+
+decrementBtn.addEventListener('click', () => {
+    document.querySelector('#value').textContent = counterValue -= 1;
+});
+
+incrementBtn.addEventListener('click', () => {
+    document.querySelector('#value').textContent = counterValue += 1;
+});
+
+
+
