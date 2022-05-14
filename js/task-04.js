ad476872d48807b9672438b59fa757a0bdc71796
MykolaHrytsenko <niksandler@gmail.com>
2022-05-14 18:51:57 +0300

    fix to task 4

--- js/task-04.js
+++ js/task-04.js
@@ -2,13 +2,17 @@ const decrementBtn = document.querySelector('button[data-action="decrement"]');
 const incrementBtn = document.querySelector('button[data-action="increment"]');
 let counterValue = 0;
 
-decrementBtn.addEventListener('click', () => {
+const decrement = () => {
     document.querySelector('#value').textContent = counterValue -= 1;
-});
+};
 
-incrementBtn.addEventListener('click', () => {
+const increment = () => {
     document.querySelector('#value').textContent = counterValue += 1;
-});
+};
+
+decrementBtn.addEventListener('click', decrement);
+
+incrementBtn.addEventListener('click', increment);
 
 
 
