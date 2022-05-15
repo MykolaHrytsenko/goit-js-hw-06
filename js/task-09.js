62f48f1fc97fdf2d652d600f3c4de3048f0c5f5f
MykolaHrytsenko <niksandler@gmail.com>
2022-05-15 18:47:15 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  bodyStyle: document.querySelector('body'),
+  colorBTN: document.querySelector('.change-color'),
+  currentColor: document.querySelector('.color')
+}
+
+function changeColor() {
+  refs.bodyStyle.style.backgroundColor = getRandomHexColor();
+  refs.currentColor.textContent = getRandomHexColor();
+}
+
+refs.colorBTN.addEventListener('click', changeColor)
+
+
