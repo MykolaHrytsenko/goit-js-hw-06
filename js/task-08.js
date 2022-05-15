572a90ea38a1320a5cc56fc172214facc1330f84
MykolaHrytsenko <niksandler@gmail.com>
2022-05-15 17:46:51 +0300

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,36 @@
+const loginForm = document.querySelector('.login-form');
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    const isFormValid = [...event.target.elements]
+        .filter((el) => el.tagName === 'INPUT')
+        .some((el) => el.value === '');
+    if (isFormValid) {
+        alert('Всі поля повинні бути заповнені!')
+    };
+
+    const email = loginForm.email.value;
+    const password = loginForm.password.value;
+
+    const formData = { email, password };
+
+    console.log(formData);
+
+    event.currentTarget.reset();
+
+    // const formData = new FormData(event.currentTarget);
+
+    // console.log(formData);
+
+    // formData.forEach((email, password) => {
+    //     console.log(`mail`, email);
+    //     console.log(`password`, password);
+    // })                                       ----- це працює якось дивно.
+
+
+};
+
+loginForm.addEventListener('submit', submitHandler);
+
 
